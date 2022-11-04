62881498816374bf98938a0292abb0240e4ff0f4
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2022-11-04 16:03:14 +0700

    mendeklarasikan variabel yang dibutuhkan

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -8,6 +8,16 @@ namespace Exercise2
 {
     class Program
     {
+        // deklarasi vaiable array
+        double[] habib = new double[44];
+        // deklarasi counter perpindahan checker dan perbandingan
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        //
+        private int n;
+
+
         
 
         static void Main(string[] args)
